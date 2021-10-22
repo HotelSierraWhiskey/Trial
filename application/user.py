5749b4b3de54350f6a31895310797d9b4222e733
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-22 04:28:25 -0400

    name to username (flask login breaks otherwise)

--- application/user.py
+++ application/user.py
@@ -3,5 +3,8 @@ from flask_login import UserMixin
 
 class User(UserMixin):
     def __init__(self, name, id):
-        self.name = name
+        self.username = name
         self.id = id
+
+    def is_authenticated(self):
+        return True
