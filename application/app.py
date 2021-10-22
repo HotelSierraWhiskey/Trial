ae96120ed24dd2a5916a73d37d2dffdad0bfbb45
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-21 22:51:51 -0400

    load_user needs to live here

--- application/app.py
+++ application/app.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_socketio import SocketIO
 from flask_cors import CORS
+from .user import User
 
 
 app = Flask(__name__)
@@ -17,3 +18,10 @@ login_manager = LoginManager(app)
 socketio = SocketIO(app)
 
 CORS(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    query = db.session.execute(f"SELECT username FROM trial.users WHERE id='{user_id}'")
+    username = query.first()[0]
+    user = User(username, user_id)
+    return user
