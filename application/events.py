98292fbcf474dc0829eaad24f9fefe3a97492e27
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-24 02:59:46 -0400

    began implementing basic room events

--- application/events.py
+++ application/events.py
@@ -1,18 +1,34 @@
 from .app import socketio
-from flask_socketio import emit
+from flask import request
+from flask_socketio import emit, join_room, leave_room
 
 
 
 @socketio.on('client_connection')
-def connection():
+def handle_connection():
     print('connect')
 
 
 @socketio.on('client_disconnection')
-def disconnection():
+def handle_disconnection():
     print('disconnect')
 
 
+@socketio.on('join_room_request')
+def handle_join_room_request(data):
+    username = data['username']
+    room_id = data['room_id']
+    join_room(room_id)
+    emit('notification', {'data': f"{username} joined room '{room_id}'"})
+
+
 @socketio.on('message')
-def message_received(data):
-    print('message')
+def handle_message(data):
+    room = data['room_id']
+    emit('message', data, to=room)
+
+
+@socketio.on('notification')
+def handle_notification(data):
+    room = data['room_id']
+    emit('notification', data, to=room)
