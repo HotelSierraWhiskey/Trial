21a1bc26b3291663ca4d5068bb9f417a86e0ee02
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-24 03:02:01 -0400

    not sure if this is correct, but it seems to work

--- application/static/room.js
+++ application/static/room.js
@@ -1,10 +1,36 @@
-function setRoomID(id) {
-    JSON.parse
-    document.getElementById("room_id").textContent = id;
+const room_id = window.location.href.split('/').pop();
+
+const username = document.getElementById('username').textContent;
+
+const room_socket = io();
+
+
+function setRoomID(room_id) {
+    document.getElementById('room_id').textContent = room_id;
 }
 
 
+room_socket.on('message', (data) => {
+    console.log('(ROOM ' + room_id + ') ', data);
+});
+
+
+room_socket.on('notification', (data) => {
+    console.log('(ROOM ' + room_id + ') ', data);
+});
+
+
+
+function requestJoinRoom(room_id) {
+    data = {'username': username, "room_id": room_id};
+    room_socket.emit('join_room_request', data);
+}
+
+
+
+
 window.onload = function() {
-    id = window.location.href.split("/").pop()
-    setRoomID(id);
+    setRoomID(room_id);
+    requestJoinRoom(room_id);
+    sio.emit('message', {'data': 'hello', 'room_id': room_id})
 }
