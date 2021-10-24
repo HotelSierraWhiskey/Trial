c90b58de1bc7e700e2842044886ad5b7e23cd8ec
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-24 03:01:04 -0400

    more work on messaging

--- application/static/events.js
+++ application/static/events.js
@@ -2,15 +2,24 @@ const sio = io();
 
 
 sio.on('connect', () => {
+  console.log("connected");
   sio.emit('client_connection');
 });
 
 
 sio.on('disconnect', () => {
+  console.log("disconnected");
   sio.emit('client_disconnection');
 });
 
 
+sio.on('message', (data) => {
+  console.log('(GLOBAL) ', data);
+});
+
+
+
 window.onload = function() {
+  
 
 }
