5ba4e32a706ea23a22b58d149b9419f6b7ba153d
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-25 20:50:29 -0400

    will make proper use of new user properties soon

--- application/user.py
+++ application/user.py
@@ -1,10 +1,25 @@
 from flask_login import UserMixin
 
 
+#   Database needs to support other values for user:
+#   email, admin, moderator, banned, member_since
+
+
 class User(UserMixin):
     def __init__(self, name, id):
         self.username = name
         self.id = id
 
+    @property
     def is_authenticated(self):
         return True
+
+    @property
+    def is_admin(self):
+        return True
+
+    @property
+    def is_moderator(self):
+        return True
+
+    
