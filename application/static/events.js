9a4d162e9723e851c9ae3f6cb4ff825d7dd432d2
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-26 14:28:33 -0400

    cleaned up

--- application/static/events.js
+++ application/static/events.js
@@ -1,8 +1,25 @@
 room_socket.on('message', (data) => {
   const username = data["username"];
   const message = data["data"];
-  const item = `<span><b class="message_username">${username}:</b><span class=message_content> ${message}</span></span><br>`;
-  document.getElementById("main_chat_box").innerHTML += item;
+
+  const link_to_user_profile = document.createElement('a');
+  link_to_user_profile.className = 'link_to_user_profile';
+  link_to_user_profile.href = `/profile/${username}`;
+  link_to_user_profile.textContent = username;
+
+  const message_content = document.createElement('span');
+  message_content.className = 'message_content';
+  message_content.textContent = `: ${message}`;
+
+  const bold = document.createElement('b');
+  bold.append(link_to_user_profile);
+
+  const whole_item = document.createElement('span');
+  whole_item.className = 'message_item'
+  whole_item.append(bold);
+  whole_item.append(message_content);
+
+  document.getElementById("main_chat_box").append(whole_item)
   document.getElementById('main_chat_box').lastChild.scrollIntoView();
 });
 
