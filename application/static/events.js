a351527ffda05022b157d69875720ebda4e3128d
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-25 20:51:27 -0400

    got rid of old notifications

--- application/static/events.js
+++ application/static/events.js
@@ -6,22 +6,8 @@ room_socket.on('message', (data) => {
 });
 
 
-// room_socket.on('notification', (data) => {
-//   const notification_type = data['type']
-//   if (notification_type == 'user_joined') {
-//       // room_socket.emit('active_users_request', {'room_id': room_id});
-//       // document.getElementById("main_chat_box").innerHTML += `${username} has joined the chat<br>`;
-//   }
-//   if (notification_type == 'user_left') {
-//       // room_socket.emit('active_users_request', {'room_id': room_id});
-//       // document.getElementById("main_chat_box").innerHTML += `${username} has left the chat<br>`;
-//   }
-//   console.log('(ROOM ' + room_id + ') ', data);
-// });
-
-
 room_socket.on('active_users_update', (data) => {
   active_users = data['active_users'];
   console.log(`active_users_update(): ${active_users}`);
-  rerenderActiveUsers();
+  renderActiveUsers();
 });
