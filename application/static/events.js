70c00fd9a4f303273ee854e572931a56f0d4dfd1
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-26 01:10:20 -0400

    added room_name_update event

--- application/static/events.js
+++ application/static/events.js
@@ -2,12 +2,16 @@ room_socket.on('message', (data) => {
   const username = data["username"];
   const message = data["data"];
   document.getElementById("main_chat_box").innerHTML += `${username} says: ${message}<br>`;
-  console.log('(ROOM ' + data['room_id'] + ') ', data);
 });
 
 
 room_socket.on('active_users_update', (data) => {
   active_users = data['active_users'];
-  console.log(`active_users_update(): ${active_users}`);
   renderActiveUsers();
+});
+
+
+room_socket.on('room_name_update', (data) => {
+  room_name = data['room_name'];
+  renderRoomName();
 });
