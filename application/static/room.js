8eb63a67d91b0e537d211c3bba1e4f3759f12b3d
HotelSierraWhiskey <winstontothemoon@gmail.com>
2021-10-26 01:10:39 -0400

    more room name update support

--- application/static/room.js
+++ application/static/room.js
@@ -1,14 +1,12 @@
 const room_id = window.location.href.split('/').pop();
 
+var room_name = '';
+
 const username = document.getElementById('username').textContent;
 
 const room_socket = io();
 
 var active_users = []
-
-
-
-function setRoomID() { document.getElementById('room_id').textContent = room_id; }
 
 
 function handleNavigateAway() { room_socket.emit('leave_room_request', {'username': username,'room_id': room_id}); }
@@ -22,6 +20,7 @@ function renderLeaveMessage() { document.getElementById("main_chat_box").innerHT
 
 function setButtons() {
     document.getElementById('chat_send_button').onclick = handleSend;
+    document.getElementById('chat_clear_chat_button').onclick = handleClearChat;
     document.getElementById('base_login').onclick = handleNavigateAway;
     document.getElementById('base_register').onclick = handleNavigateAway;
     document.getElementById('base_home').onclick = handleNavigateAway;
@@ -35,6 +34,11 @@ function requestJoinRoom() {
 }
 
 
+function requestRoomName() {
+    room_socket.emit('room_name_request', {'room_id': room_id});
+}
+
+
 function renderActiveUsers() {
     document.getElementById('users_in_room_list').innerHTML = ''
     for (var i = 0; i < active_users.length; i++) {
@@ -42,6 +46,9 @@ function renderActiveUsers() {
         document.getElementById('users_in_room_list').innerHTML += `<li id=${entry}>${entry}</li>`;
     }
 }
+
+
+function renderRoomName() { document.getElementById('room_name').textContent = room_name; } 
 
 
 function handleSend() {
@@ -54,10 +61,15 @@ function handleSend() {
 }
 
 
+function handleClearChat() {
+    document.getElementById('main_chat_box').textContent = '';
+}
+
+
 window.onload = function() {
-    setRoomID();
     setButtons();
     requestJoinRoom();
+    requestRoomName();
 
     document.getElementById("chat_message_text_area").addEventListener("keydown", (e) => {
         if (e.key === "Enter") {
